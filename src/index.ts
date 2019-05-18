bb41435ba5e6faebb56d1d75e0a6a30f5b1b73e9
godzzolp <godzzo@lightport.hu>
2019-05-18 13:12:26 +0200

    First tryout - succeeded

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-console.log("Loaded!");
+
+import gsjson = require('google-spreadsheet-to-json');
+
+gsjson({
+	spreadsheetId: '1A-CnEIWo4YUtYWqw8QZGkfOJzzw0TBXEL1kll3C9nbE',
+	credentials: "credentials/gd-drive-access-a930341b5052.json"
+    // other options...
+})
+.then(function(result: any) {
+    console.log(result.length);
+    console.log(result);
+})
+.catch(function(err: any) {
+    console.log(err.message);
+    console.log(err.stack);
+});
+
 export const NgModelGen = (name: string) => 'Hello '+name;
